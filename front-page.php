f186613f85ecdd427863f7f503fafd7b91b1e548
sm4x <max.schattauer@gmail.com>
2020-06-10 13:36:48 +0200

    update

--- front-page.php
+++ front-page.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
-  <div class="container">
-    <h1><?php the_title(); ?></h1>
-    <?php get_template_part('inc/section','content'); ?>
-  </div>
+  <section class="page-wrap">
+
+    <div class="container">
+      <h1><?php the_title(); ?></h1>
+      <?php get_template_part('inc/section','content'); ?>
+    </div>
+  </section>
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+  // Load Style Sheets
   function load_css(){
     wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all' );
     wp_enqueue_style('bootstrap');
@@ -6,13 +7,16 @@
     wp_register_style('main', get_template_directory_uri() . '/css/main.css', array(), false, 'all' );
     wp_enqueue_style('main');
   }
+  add_action('wp_enqueue_scripts', 'load_css');
 
+  // Load Java Script
   function load_js(){
     wp_enqueue_script('jquery');
     wp_register_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery', false, true);
     wp_enqueue_script('bootstrap');
   }
-
-  add_action('wp_enqueue_scripts', 'load_css');
   add_action('wp_enqueue_scripts', 'load_js');
+
+  // Theme Options
+  add_theme_support('menus');
 ?>
