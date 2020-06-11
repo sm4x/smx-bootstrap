2dadd997ea3ad2743e148e9ba974975d51e49b7c
sm4x <max.schattauer@gmail.com>
2020-06-11 13:09:38 +0200

    update

--- inc/section-archive.php
+++ inc/section-archive.php
@@ -1,4 +1,5 @@
 <?php if ( have_posts() ): while( have_posts() ): the_post(); ?>
   <h3><?php the_title(); ?></h3>
   <?php the_excerpt(); ?>
+  <a href="<?php the_permalink(); ?>">Read more</a>
 <?php endwhile; else: endif; ?>
