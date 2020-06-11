f7ac6ee9874d68e2cd87d9e52a9e096d72818719
sm4x <max.schattauer@gmail.com>
2020-06-11 16:03:28 +0200

    update

--- single.php
+++ single.php
@@ -4,6 +4,11 @@
       <h1><?php the_title(); ?></h1>
       <div class="row">
         <div class="col-md-9 col-md-push-3">
+          <?php if (has_post_thumbnail)
+            {
+              echo('This has a featured image');
+            }
+          ?>
           <?php get_template_part('inc/section','blogcontent'); ?>
           <?php wp_link_pages(); ?>
         </div>
