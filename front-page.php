bc1eb4f75595d8263688351d8261f3ac77eb3612
sm4x <max.schattauer@gmail.com>
2020-06-11 09:20:53 +0200

    update

--- front-page.php
+++ front-page.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
-  <section class="page-wrap">
-
-    <div class="container">
-      <h1><?php the_title(); ?></h1>
-      <?php get_template_part('inc/section','content'); ?>
-    </div>
-  </section>
+    <main>
+      <section class="page-wrap">
+        <div class="container">
+          <h1><?php the_title(); ?></h1>
+          <?php get_template_part('inc/section','content'); ?>
+        </div>
+      </section>
+    </main>
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,6 +1,10 @@
 <?php get_header(); ?>
-  <div class="container">
-    <h1><?php the_title(); ?></h1>
-    <?php get_template_part('inc/section','content'); ?>
-  </div>
+    <main>
+      <section class="page-wrap">
+        <div class="container">
+          <h1><?php the_title(); ?></h1>
+          <?php get_template_part('inc/section','content'); ?>
+        </div>
+      </section>
+    </main>
 <?php get_footer(); ?>
