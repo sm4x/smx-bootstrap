ce5fcdde7f15c188d7796dad56a6968d02b26166
sm4x <max.schattauer@gmail.com>
2020-06-11 14:03:49 +0200

    update

--- inc/section-blogcontent.php
+++ inc/section-blogcontent.php
@@ -1,13 +1,14 @@
 <?php if ( have_posts() ): while( have_posts() ): the_post(); ?>
   <?php
-    // print author, name and date
-    /*
+    // print author, name, date and tags
+
     the_author();
     $fname = get_the_author_meta('first_name');
     $lname = get_the_author_meta('last_name');
     echo ($fname . ' ' . $lname);
     echo (get_the_date('d/m/Y'));
-    */
+    $tags = get_the_tags();
+    echo ($tags);
   ?>
   <?php the_content(); ?>
 <?php endwhile; else: endif; ?>
