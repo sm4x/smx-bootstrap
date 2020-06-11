c90fa00365c6fcd6a1764d13a38102eccf276f35
sm4x <max.schattauer@gmail.com>
2020-06-11 13:37:42 +0200

    update

--- archive.php
+++ archive.php
@@ -4,8 +4,17 @@
       <div class="row">
         <div class="col-md-9 col-md-push-3">
           <?php get_template_part('inc/section','archive'); ?>
-          <?php previous_post_link(); ?>
-          <?php next_post_link(); ?>
+          <?php
+            global $wp_query;
+            $big = 999999999; // unlikely integer
+            echo paginate_links( [
+              'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ))),
+              'format' => '?page=%#%',
+              'current' => max( 1, get_query_var('paged') ),
+              'total' => $wp_query->max_num_pages,
+              ]
+            )
+          ?>
         </div>
         <div class="col-md-3 col-md-pull-9" id="sidebar"><?php get_sidebar();?></div>
       </div>
