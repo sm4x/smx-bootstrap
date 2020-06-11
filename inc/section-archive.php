0edbcaec68f52544a993a2be2c64554a0904bec6
sm4x <max.schattauer@gmail.com>
2020-06-11 13:13:19 +0200

    update

--- inc/section-archive.php
+++ inc/section-archive.php
@@ -1,5 +1,9 @@
 <?php if ( have_posts() ): while( have_posts() ): the_post(); ?>
-  <h3><?php the_title(); ?></h3>
-  <?php the_excerpt(); ?>
-  <a href="<?php the_permalink(); ?>">Read more</a>
+  <div class="card mb-3">
+    <div class="card-body">
+      <h3><?php the_title(); ?></h3>
+      <?php the_excerpt(); ?>
+      <a href="<?php the_permalink(); ?>" class=btn btn-success>Read more</a>
+    </div>
+  </div>
 <?php endwhile; else: endif; ?>
