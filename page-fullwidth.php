1edda3665569f4cbd4c02244a29cbf1fc9a268f8
sm4x <max.schattauer@gmail.com>
2020-06-11 09:22:52 +0200

    update

--- page-fullwidth.php
+++ page-fullwidth.php
@@ -1,3 +1,12 @@
+<?php
+/**
+* Template Name: Full Width Page
+*
+* @package WordPress
+* @subpackage smx-bootstrap
+* @since 0.1
+*/
+?>
 <?php get_header(); ?>
     <main>
       <?php get_template_part('inc/section','content'); ?>
