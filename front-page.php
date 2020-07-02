1d5a7dacfe091b1e6093f07588bd9048bf34d17b
sm4x <max.schattauer@gmail.com>
2020-07-02 11:37:40 +0200

    update

--- front-page.php
+++ front-page.php
@@ -12,9 +12,11 @@
         <div class="col-lg-9">
           <h1><?php the_title(); ?></h1>
           <?php get_template_part('inc/section','content'); ?>
+          <?php get_search_form(); ?>
         </div>
+
       </div>
-      <?php get_search_form(); ?>
+
     </section>
   </main>
 <?php get_footer(); ?>
