1b5d8be654a645de98ae700cb0c3d2d239bfd9b2
sm4x <max.schattauer@gmail.com>
2020-07-02 16:42:57 +0200

    update

--- single-produkte.php
+++ single-produkte.php
@@ -1,4 +1,8 @@
-<?php get_header(); ?>
+<?php get_header();
+/*
+Display Custom Post Type 'Produkte'
+*/
+?>
   <main>
     <section class="page-wrap">
       <h1><?php the_title(); ?></h1>
@@ -15,7 +19,7 @@
           <?php if (has_post_thumbnail): // DISPLAY IMAGE ?>
             <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title();?>" class="img-fluid mb-3 img-thumbnail">
           <?php endif; ?>
-          <?php get_template_part('inc/section','blogcontent'); ?>
+          <?php get_template_part('inc/section','produkte'); ?>
           <?php wp_link_pages(); // Link Pages that are separated with nextpage tag?>
         </div>
       </div>
