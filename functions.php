bea1c3c3c0b2a535d54504039cc1b550c46c2891
sm4x <max.schattauer@gmail.com>
2020-07-08 11:06:28 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -22,6 +22,7 @@
   add_theme_support('post-thumbnails');
   add_theme_support('widgets');
   add_theme_support('custom-logo');
+  add_theme_support('title-tag');
 
   // Menus
   register_nav_menus(
